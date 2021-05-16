21926149aa28becbba8d17f3e8458fbd26e60b8f
Jeserlin <jeserlin@me.com>
2021-05-16 23:10:29 +0800

    Define font family

--- theme/index.js
+++ theme/index.js
@@ -1,9 +1,11 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
+import typography from './typography';
 import palette from './palette';
 import mixins from './mixins';
 
 export default createMuiTheme({
+  typography,
   palette,
   mixins,
 });
---
+++ theme/typography.js
@@ -0,0 +1,86 @@
+const commonFontFamily = '"Indie Flower", cursive';
+const captionFontFamily = `"PTSans-Caption", ${commonFontFamily}`;
+
+export default {
+  fontFamily: commonFontFamily,
+  h1: {
+    fontFamily: captionFontFamily,
+    fontSize: 28,
+    fontWeight: 'bold',
+    lineHeight: 1.29,
+  },
+  h2: {
+    fontFamily: captionFontFamily,
+    fontSize: 28,
+    fontWeight: 'normal',
+    lineHeight: 1.29,
+  },
+  h3: {
+    fontFamily: captionFontFamily,
+    fontSize: 24,
+    fontWeight: 'bold',
+    lineHeight: 1.33,
+  },
+  h4: {
+    fontFamily: captionFontFamily,
+    fontSize: 24,
+    fontWeight: 'normal',
+    lineHeight: 1.33,
+  },
+  h5: {
+    fontFamily: commonFontFamily,
+    fontSize: 30,
+    fontWeight: 'bold',
+    lineHeight: 1.3,
+  },
+  h6: {
+    fontFamily: commonFontFamily,
+    fontSize: 20,
+    fontWeight: 'normal',
+    lineHeight: 1.4,
+  },
+  subtitle1: {
+    fontFamily: commonFontFamily,
+    fontSize: 16,
+    fontWeight: 'bold',
+    lineHeight: 1.25,
+  },
+  subtitle2: {
+    fontFamily: commonFontFamily,
+    fontSize: 14,
+    fontWeight: 'bold',
+    lineHeight: 1.29,
+  },
+  body1: {
+    fontFamily: commonFontFamily,
+    fontSize: 16,
+    fontWeight: 'normal',
+    lineHeight: 1.25,
+  },
+  body2: {
+    fontFamily: commonFontFamily,
+    fontSize: 14,
+    fontWeight: 'normal',
+    lineHeight: 1.14,
+  },
+  button: {
+    fontFamily: commonFontFamily,
+    fontSize: 16,
+    fontWeight: 'bold',
+    lineHeight: 1.25,
+    textTransform: null,
+  },
+  caption: {
+    fontFamily: commonFontFamily,
+    fontSize: 14,
+    fontWeight: 'normal',
+    lineHeight: 1.14,
+  },
+  overline: {
+    fontFamily: commonFontFamily,
+    fontSize: 14,
+    fontWeight: 'bold',
+    lineHeight: 1.29,
+    textTransform: null,
+  },
+};
