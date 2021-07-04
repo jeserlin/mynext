066061bb93bceaffb8dde6ea4ce4a26617dc6c12
Jeserlin <jeserlin@me.com>
2021-07-04 21:21:50 +0800

    Add routers in sideNav.

---
+++ pages/baking/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Baking = () => <div>Baking</div>;
+
+export default Baking;
---
+++ pages/cooking/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Cooking = () => <div>Cooking</div>;
+
+export default Cooking;
