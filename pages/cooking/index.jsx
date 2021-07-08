95f43a8c518bcac924a7641a875a895755d5f858
Jeserlin <jeserlin@me.com>
2021-07-08 13:50:35 +0800

    Display cooking & tech posts in main.

--- pages/cooking/index.jsx
+++ pages/cooking/index.jsx
@@ -1,5 +1,48 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { Typography } from '@material-ui/core';
 
-const Cooking = () => <div>Cooking</div>;
+import { getPostsByFolder } from '../../lib/api';
+
+const propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    slug: PropTypes.string,
+    title: PropTypes.string,
+  })),
+};
+
+const defaultProps = {
+  posts: [],
+};
+
+const Cooking = ({ posts }) => (
+  <>
+    {posts.map(({ slug, title }) => (
+      <Typography key={slug} color="textSecondary">
+        <Link href={`/cooking/${slug}`}>
+          <a>{title}</a>
+        </Link>
+      </Typography>
+    ))}
+  </>
+);
+
+export async function getStaticProps() {
+  const posts = getPostsByFolder({
+    folder: 'cooking',
+    fields: ['slug', 'title'],
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+Cooking.propTypes = propTypes;
+Cooking.defaultProps = defaultProps;
 
 export default Cooking;
--- pages/tech/[slug].jsx
+++ pages/tech/[slug].jsx
@@ -1,7 +1,145 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ErrorPage from 'next/error';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { Grid, Typography, makeStyles } from '@material-ui/core';
 
-const TechPost = () => (
-  <div>Tech post</div>
-);
+import markdownToHtml from '../../lib/markdownToHtml';
+import { getPostBySlug, getPostsByFolder } from '../../lib/api';
+
+const propTypes = {
+  post: PropTypes.shape({
+    slug: PropTypes.string,
+    title: PropTypes.string,
+    content: PropTypes.string,
+    coverImage: PropTypes.string,
+  }),
+};
+
+const defaultProps = {
+  post: {},
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  title: {
+    marginBottom: theme.spacing(4),
+  },
+  content: {
+    order: 2,
+    [theme.breakpoints.up('md')]: {
+      order: 1,
+    },
+  },
+  image: {
+    order: 1,
+    [theme.breakpoints.up('md')]: {
+      order: 2,
+    },
+    '& > * > img': {
+      borderRadius: theme.spacing(4),
+    },
+  },
+  markdown: {
+    '& > h3': {
+      ...theme.typography.h3,
+      color: theme.palette.primary.dark,
+    },
+    '& > h4': {
+      ...theme.typography.h4,
+      color: theme.palette.secondary.dark,
+    },
+    '& > ul,li,ol': {
+      ...theme.typography.body1,
+      color: theme.palette.text.secondary,
+      paddingBottom: theme.spacing(1.5),
+    },
+  },
+}));
+
+const TechPost = ({ post }) => {
+  const classes = useStyles();
+
+  const router = useRouter();
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />;
+  }
+
+  return (
+    <>
+      <Typography
+        variant="h1"
+        color="textSecondary"
+        className={classes.title}
+      >
+        {post.title}
+      </Typography>
+      <Grid container className={classes.root}>
+        <Grid item xs={12} md={8} className={classes.content}>
+          <div
+            className={classes.markdown}
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+        </Grid>
+        <Grid item xs={12} md={4} className={classes.image}>
+          {post.coverImage
+            ? (
+              <Image
+                src={post.coverImage}
+                layout="responsive"
+                width="100"
+                height="100"
+              />
+            )
+            : ''}
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export async function getStaticProps({ params }) {
+  const post = getPostBySlug('tech', params.slug, [
+    'title',
+    'date',
+    'slug',
+    'content',
+    'ogImage',
+    'coverImage',
+  ]);
+  const content = await markdownToHtml(post.content || '');
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = getPostsByFolder({
+    folder: 'tech',
+    fields: ['slug'],
+  });
+
+  return {
+    paths: posts.map((post) => ({
+      params: {
+        slug: post.slug,
+      },
+    })),
+    fallback: false,
+  };
+}
+
+TechPost.propTypes = propTypes;
+TechPost.defaultProps = defaultProps;
 
 export default TechPost;
