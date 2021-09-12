60ec7784df6cd2268815e1babfeaf7985889f384
Jeserlin <jeserlin@me.com>
2021-09-12 17:19:11 +0800

    feat: customize 404 page.

---
+++ pages/404.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Image from 'next/image';
+import {
+  Box, Grid, Typography, fade, makeStyles,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: '100%',
+  },
+  title: {
+    color: theme.palette.primary.dark,
+    margin: theme.spacing(4, 0, 2, 0),
+  },
+}));
+
+const NotFound = () => {
+  const classes = useStyles();
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      className={classes.container}
+    >
+      <Image
+        src="/assets/others/myBunnies.png"
+        width={200}
+        height={200}
+      />
+      <Typography
+        variant="h6"
+        className={classes.title}
+      >
+        Oops, page not found
+      </Typography>
+      <Typography color="textSecondary">
+        But we have some cute bunnies for you, enjoy ~ !
+      </Typography>
+    </Box>
+  );
+};
+
+export default NotFound;
